5a62003e1ecd4d1ce8051b27b655a5379f282424
Gabriel O. Costa <g.oliveiralk@gmail.com>
2022-11-25 21:02:30 -0300

    Sistema Intranet

--- frontend/src/global.js
+++ frontend/src/global.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 
 export const userKey = '__knowledge_users'
-export const baseApiUrl = 'http://192.168.0.100:3000'
+
+export const baseApiUrl = process.env.PORT
+// export const baseApiUrl = 'http://192.168.0.100:3000'
 
 export function showError(e) {
     if(e && e.response && e.response.data) {
